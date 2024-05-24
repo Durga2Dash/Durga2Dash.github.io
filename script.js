578b9d7b1d851074ded3721103adc532105bd6ad
Durga2Dash <25481987+Durga2Dash@users.noreply.github.com>
2024-05-24 15:30:19 -0400

    Update

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ function updateScatterChart() {
 setInterval(updateScatterChart, 4000); // Update scatter plot every 4 seconds
 
 document.addEventListener('DOMContentLoaded', function() {
-    const roles = ["Data Science", "AI/ML Engineering (Structured Data & NLP)", "Cloud Site Reliability Engineering", "DevOps", "Laughing at My Own Jokes", "Winning Imaginary Arguments", "Napping"];
+    const roles = ["Data Science", "AI/ML Engineering (Structured Data & NLP)", "Cloud Site Reliability Engineering", "DevOps", "Business Strategy", "Laughing at My Own Jokes", "Winning Imaginary Arguments", "Napping"];
     let currentRoleIndex = 0;
     const roleElement = document.getElementById('role-changer');
 
